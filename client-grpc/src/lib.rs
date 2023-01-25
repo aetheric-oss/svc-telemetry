288d545dddf7242d4e7b9fd6ccedfdfb1581b8ac
AM Smith <smitham42@gmail.com>
2023-01-24 17:53:51 -0800

    fix: address peer review

--- client-grpc/src/lib.rs
+++ client-grpc/src/lib.rs
@@ -2,11 +2,11 @@
 //! <img src="https://github.com/Arrow-air/tf-github/raw/main/src/templates/doc-banner-services.png" style="height:250px" />
 //! </center>
 //! <div align="center">
-//!     <a href="https://github.com/Arrow-air/lib-ccsds/releases">
-//!         <img src="https://img.shields.io/github/v/release/Arrow-air/lib-ccsds?include_prereleases" alt="GitHub release (latest by date including pre-releases)">
+//!     <a href="https://github.com/Arrow-air/svc-telemetry/releases">
+//!         <img src="https://img.shields.io/github/v/release/Arrow-air/svc-telemetry?include_prereleases" alt="GitHub release (latest by date including pre-releases)">
 //!     </a>
-//!     <a href="https://github.com/Arrow-air/lib-ccsds/tree/main">
-//!         <img src="https://github.com/arrow-air/lib-ccsds/actions/workflows/rust_ci.yml/badge.svg?branch=main" alt="Rust Checks">
+//!     <a href="https://github.com/Arrow-air/svc-telemetry/tree/main">
+//!         <img src="https://github.com/arrow-air/svc-telemetry/actions/workflows/rust_ci.yml/badge.svg?branch=main" alt="Rust Checks">
 //!     </a>
 //!     <a href="https://discord.com/invite/arrow">
 //!         <img src="https://img.shields.io/discord/853833144037277726?style=plastic" alt="Arrow DAO Discord">
