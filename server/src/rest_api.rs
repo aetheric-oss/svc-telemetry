8cb35078d6adf7c5b8c7332486adb011d92bf902
AM Smith <smitham42@gmail.com>
2023-01-24 17:14:08 -0800

    chore: add mavlink endpoint

--- server/src/rest_api.rs
+++ server/src/rest_api.rs
@@ -1,13 +1,19 @@
 //! REST API implementations for svc-telemetry
 
+use crate::cache::RedisPool;
 use crate::grpc_clients::GrpcClients;
 use axum::{extract::Extension, Json};
+use snafu::prelude::Snafu;
+use std::cmp::Ordering;
 
 /// Types Used in REST Messages
 pub mod rest_types {
     include!("../../openapi/types.rs");
 }
-pub use rest_types::AdsbPacket;
+
+pub use mavlink::{common::MavMessage, MavFrame, MavlinkVersion, Message};
+
+pub use rest_types::{Keys, MavlinkMessage};
 
 // /// Writes an info! message to the app::req logger
 // macro_rules! req_info {
@@ -17,11 +23,11 @@ pub use rest_types::AdsbPacket;
 // }
 
 // /// Writes an error! message to the app::req logger
-// macro_rules! req_error {
-//     ($($arg:tt)+) => {
-//         log::error!(target: "app::req", $($arg)+);
-//     };
-// }
+macro_rules! req_error {
+    ($($arg:tt)+) => {
+        log::error!(target: "app::req", $($arg)+);
+    };
+}
 
 /// Writes a debug! message to the app::req logger
 macro_rules! req_debug {
@@ -30,24 +36,94 @@ macro_rules! req_debug {
     };
 }
 
-/// Post ADS-B Telemetry
+#[derive(Debug, Snafu)]
+enum ProcessError {
+    #[snafu(display("Could not parse the packet."))]
+    CouldNotParse,
+
+    #[snafu(display("Could not write to the cache."))]
+    CouldNotWriteCache,
+}
+
+/// Parses a Mavlink packet from bytes and reports the number of times
+///  this specific packet has been received
+async fn process_mavlink(
+    payload: &[u8],
+    mut cache: RedisPool,
+) -> Result<(MavFrame<MavMessage>, i64), ProcessError> {
+    req_debug!("(process_mavlink) entry");
+
+    let Ok(frame) = MavFrame::<MavMessage>::deser(MavlinkVersion::V2, payload) else {
+        return Err(ProcessError::CouldNotParse);
+    };
+
+    let key: u32 = frame.header().hashed_key();
+
+    // Set the key
+    let result = cache.try_key(key).await;
+    let Ok(count) = result else {
+        req_error!("{}", result.unwrap_err());
+        return Err(ProcessError::CouldNotWriteCache);
+    };
+
+    Ok((frame, count))
+}
+
+/// Post Mavlink Telemetry
+/// Min 8 bytes, max 263 bytes
 #[utoipa::path(
     post,
-    path = "/telemetry/adsb",
+    path = "/telemetry/mavlink/adsb",
     tag = "svc-telemetry",
-    request_body = AdsbPacket,
+    request_body = MavlinkMessage,
     responses(
         (status = 200, description = "Telemetry received."),
+        (status = 400, description = "Malformed packet."),
+        (status = 500, description = "Something went wrong."),
     )
 )]
-pub async fn adsb(
+pub async fn mavlink_adsb(
+    Extension(mavlink_cache): Extension<RedisPool>,
     Extension(mut _grpc_clients): Extension<GrpcClients>,
-    Json(_payload): Json<AdsbPacket>,
-) -> Result<Json<bool>, (hyper::StatusCode, String)> {
-    req_debug!("(adsb) entry");
+    Json(payload): Json<MavlinkMessage>,
+) -> Result<Json<i64>, hyper::StatusCode> {
+    req_debug!("(mavlink_adsb) entry");
 
-    // TODO Push to svc-storage
-    // TODO Push to third party node
+    let result = process_mavlink(&payload.bytes, mavlink_cache).await;
+    let Ok((_frame, count)) = result else {
+        match result {
+            Err(ProcessError::CouldNotParse) => {
+                return Err(hyper::StatusCode::BAD_REQUEST);
+            },
+            _ => {
+                return Err(hyper::StatusCode::INTERNAL_SERVER_ERROR);
+            }
+        };
+    };
 
-    Ok(Json(true))
+    match count.cmp(&1) {
+        Ordering::Less => {
+            req_debug!(
+                "(mavlink_adsb) ADS-B report count should be impossible: {}.",
+                count
+            );
+            return Err(hyper::StatusCode::INTERNAL_SERVER_ERROR);
+        }
+        Ordering::Equal => {
+            req_debug!("(mavlink_adsb) first time this ADS-B packet was received.");
+            // write raw packet to svc-storage
+        }
+        _ => {
+            req_debug!(
+                "(mavlink_adsb) confirmations received for this ADS-B packet: {}",
+                count
+            );
+            // increment confirmations to svc-storage?
+        }
+    };
+
+    // Push to svc-storage
+    // Push to third-party
+    req_debug!("(mavlink_adsb) success");
+    Ok(Json(count))
 }
